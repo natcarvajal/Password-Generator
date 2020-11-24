308e13669eea1e819541937573043456b8663db4
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2020-11-24 17:06:27 -0500

    finalized project with working generator

--- script.js
+++ script.js
@@ -1,55 +1,140 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // // // different variables for password
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numericChars = ["0","1","2","3","4","5","6","7","8","9"];
-var specialChars = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","~","`","{","[","}","]","|",";",":","<",">","?"];
+var lowerCaseAr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCaseArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numericCharsArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharsArr = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "=",
+  "+",
+  "~",
+  "{",
+  "[",
+  "}",
+  "]",
+  "|",
+  ";",
+  ":",
+  "<",
+  ">",
+  "?",
+];
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
 // Write password to the #password input
 
-function generatePassword(){
-  var passLength = prompt("Please choose a password length between 8 and 128 characters.");
-    if (passLength >= 8 && passLength <= 128) {
+function generatePassword() {
+  var passLength = prompt(
+    "Please choose a password length between 8 and 128 characters."
+  );
+
+  if (passLength >= 8 && passLength <= 128) {
     alert("Perfect, please continue");
     // WHEN prompted for password criteria
     // THEN I select which criteria to include in the password
     var lowerCase = confirm("Would you like to use any lower case letters?");
     var upperCase = confirm("Would you like to use any upper case letters?");
-    var numericChars = confirm("Would you like to use any numbers?"); 
-    var specialChars = confirm("Would you like to use any special characeters? ");   
-  
+    var numericChars = confirm("Would you like to use any numbers?");
+    var specialChars = confirm(
+      "Would you like to use any special characeters? "
+    );
+
     var password = "";
-    var nextChar = "";
     var combinedArray = [];
 
     // if/else statements//
-    if (hasLowerCase) {
-      combinedArray.concat(lowerCase);
+    if (lowerCase) {
+      combinedArray.push(lowerCaseAr);
     }
-    if (hasUpperCase){
-    combinedArray.concat(upperCase);
+    if (upperCase) {
+      combinedArray.push(upperCaseArr);
     }
-    if (hasNumericChars){
-      combinedArray.concat(numericChars);
+    if (numericChars) {
+      combinedArray.push(numericCharsArr);
     }
-    if (hasSpecialChars){
-      combinedArray.concat(specialChars);
+    if (specialChars) {
+      combinedArray.push(specialCharsArr);
     }
     // WHEN all prompts are answered
     // THEN a password is generated that matches the selected criteria
-    for (i = 0; i < passLength; i++){
-    nextChar = Math.floor(Math.random(passLength.combinedArray));
-    password.concat(nextChar);
+    for (i = 0; i < passLength; i++) {
+      next_array =
+        combinedArray[Math.floor(Math.random() * combinedArray.length)];
+      password += nextArr[Math.floor(Math.random() * next_array.length)];
     }
     return password;
-    }else{
+  } else {
     alert("Thats not a valid length, try again.");
-    }
   }
+}
 
 function writePassword() {
   var password = generatePassword();
